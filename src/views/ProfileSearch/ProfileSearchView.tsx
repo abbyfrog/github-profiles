6af9353b25ad7851322352e2bdbb8543c2a0e7e2
abbyfrog <abbyjameson@me.com>
2022-08-24 18:14:50 +1200

    Add search field and retrieve GitHub profile on click/enter
    Add an `IconButton` and handle clicks on that button (or 'enter'
    key-presses) by calling `searchProfiles`. This callback then
    makes an API request to the GitHub API to retrieve the profile
    that matches the specified username.
    
    Also, handle loading and error states by setting these appropriately
    and displaying their representation on the UI if they are set.

--- src/views/ProfileSearch/ProfileSearchView.tsx
+++ src/views/ProfileSearch/ProfileSearchView.tsx
@@ -1,4 +1,71 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Card,
+  CardHeader,
+  CircularProgress,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from '@mui/material';
+import { Search } from '@mui/icons-material';
 
-export const ProfileSearchView: React.FC = () => <Typography>GitHub profile finder</Typography>;
+export const ProfileSearchView: React.FC = () => {
+  const [searchValue, setSearchValue] = React.useState<string>('');
+
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>(undefined);
+
+  const searchProfiles = React.useCallback((profileToSearchFor: string) => {
+    setLoading(true);
+    setError(undefined);
+
+    fetch(`https://api.github.com/users/${profileToSearchFor}`)
+      .then(async (data) => {
+        const jsonUser: unknown = await data.json();
+        console.log('User: ', jsonUser);
+      })
+      .catch((err: unknown) => {
+        const errorMessage = `Error fetching user: ${JSON.stringify(err, null, 2)}`;
+        console.error(errorMessage);
+        setError(errorMessage);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <Card>
+      <CardHeader title="GitHub profile finder" />
+      <TextField
+        label="Username"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            searchProfiles(searchValue);
+          }
+        }}
+        variant="outlined"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton type="submit" onClick={() => searchProfiles(searchValue)}>
+                <Search />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      {loading && <CircularProgress />}
+      {error && (
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error}
+        </Alert>
+      )}
+    </Card>
+  );
+};
